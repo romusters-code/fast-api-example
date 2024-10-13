a62940e3e8bb187e5e86623b570df6967c54c565
Robert Musters <romusters@gmail.com>
2024-10-13 17:31:15 +0200

    Remove unused import and add documentation

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-import json
 from model import Handler
 from pydantic import BaseModel
 
@@ -21,14 +20,28 @@ handler = Handler()
 
 @app.post("/embed")
 async def embed_text(text_input: TextInput) -> EmbeddingOutput:
+    """
+    Endpoint to creating embeddings from the input text.
+
+    :param text_input: The text to embed.
+    :return: The embedding of the input text as JSON.
+    """
     embedding = handler.embed(text_input.text)
     return EmbeddingOutput(embedding=embedding, description="The list of float values representing the text embedding.")
 
 
 @app.post("/similarity")
 async def calculate_similarity(text_1: TextInput, text_2: TextInput) -> SimilarityOutput:
+    """
+    Compute the cosine similarity between two input texts.
+
+    :param text_1: The first text.
+    :param text_2: The second text.
+    :return: The similarity score between the two input texts as JSON.
+    """
     similarity_score = handler.similarity(text_1=text_1.text, text_2=text_2.text)
-    return SimilarityOutput(similarity=similarity_score, description="Value explaining similarity.")
+    return SimilarityOutput(similarity=similarity_score, description=
+        "Cosine similarity indicating semantic similarity. A value close to 1.0 is very similar, close to 0.0 close to -1.0 means little to no similarity, is very dissimilar.")
 
 
 if __name__ == "__main__":
